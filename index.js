e07ae668e843258948519b77a439d9fc6832b7af
Venkysanju246 <sanjuvenky246@gmail.com>
2023-04-03 16:27:00 +0530

    hhireork bot

--- index.js
+++ index.js
@@ -1,33 +1,34 @@
-const {Client} = require("whatsapp-web.js");
+const { Client } = require("whatsapp-web.js");
 const qrcode = require("qrcode-terminal");
-const {Configuration, OpenAIApi} = require("openai");
+const { Configuration, OpenAIApi } = require("openai");
+require("dotenv").config()
 
 const client = new Client();
 
-client.on('qr',(qr) =>{
-    qrcode.generate(qr,{small:true});
+client.on('qr', (qr) => {
+    qrcode.generate(qr, { small: true });
 });
 
-client.on('ready',() =>{
+client.on('ready', () => {
     console.log("Client is ready");
 });
 
 client.initialize();
 
 const configuration = new Configuration({
-    apiKey : "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",
+    apiKey: process.env.SECRET_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
-async function runCompletion(message){
+async function runCompletion(message) {
     const completion = await openai.createCompletion({
-        model:"text-davinci-003",
+        model: "text-davinci-003",
         prompt: message,
         max_tokens: 2500,
     });
     return completion.data.choices[0].text;
 }
-client.on('message',message => {
+client.on('message', message => {
     console.log(message.body);
     runCompletion(message.body).then(result => message.reply(result))
 })
